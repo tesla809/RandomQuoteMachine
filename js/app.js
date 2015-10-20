48a466c48bec8599b37a6f6ed63f7275159b707e
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-10-20 13:42:17 -0400

    added ability to output random languages (latin, korean, thai, arabic) with random num of characters

--- js/app.js
+++ js/app.js
@@ -1,1 +1,69 @@
 //app.js
+
+
+//create random text generation in quote area
+
+    var $description  = "<div class='jumbotron description'>"
+    	$description +=	"<div class='description'><p>Here are some random quotes that I have stolen from Twitter.";
+        $description += "</br>I hope you enjoy them as much as I enjoyed stealing them.</p></div>";
+        $description += "</div>";
+
+
+
+
+
+
+// create language object constructor function-
+function Language(startLang, endLang, langName){    
+    this.startLang = startLang;
+    this.endLang = endLang;
+    this.langName = langName;
+} 
+
+// function for random language pick-
+function randomLanguage(){
+    var languageArray = [];
+
+    // add language with ranges
+    var arabic = new Language(0x060C,0x06FE,"arabic");
+    var thai = new Language(0x0E01, 0x0E5B, "thai");
+    var korean = new Language(0x1100, 0x11F9, "korean");
+    var latin = new Language(0x0020, 0x007E, "latin");
+    
+    languageArray.push(latin);
+    languageArray.push(arabic);
+    languageArray.push(thai);
+    languageArray.push(korean);
+    
+    var randomPickStart = Math.floor(Math.random() * languageArray.length);
+    var randomPickEnd = randomPickStart;
+    
+    console.log("randomPickStart: " + randomPickStart,"randomPickEnd: " + randomPickEnd);
+    
+    return {
+        start: languageArray[randomPickStart].startLang,
+        end: languageArray[randomPickEnd].endLang  
+    };
+    
+}
+
+// randomText called from array of language options-
+// allow randomText to accept language-
+function randomText(start, end){
+    var output = "";
+    var randomNumberOfLetters = Math.floor(Math.random() * 10);
+    
+    for(var i = 0; i < randomNumberOfLetters; i++){
+        var text = String.fromCharCode(start + Math.random() * (end-start+1));
+        output += text;
+    }
+    return output;
+}
+
+// console.log(randomText(randomLanguage().start, randomLanguage().end));
+
+
+$(document).ready(function(){
+	$(".random-cool-text").text("<p>Hello</p>");
+
+});
